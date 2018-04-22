e2a9772fd8702b104da4b6eeb426905cd8ea2ea6
Eduardo Zimelewicz <eduardo.zimelewicz@gmail.com>
2018-04-22 13:02:00 -0300

    parallel assignment

--- k_means.c
+++ k_means.c
@@ -1,152 +1,173 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include "mpi.h"
+#include<stddef.h>
 #include<math.h>
-
-typedef struct point {
-    int id;
-    double point_x;
-    double point_y;
-    double ctr_dist;
-    int cluster_id;
-} T_point;
-
-typedef struct cluster {
-    double centr_x;
-    double centr_y;
-    int cluster_id;
-} T_cluster;
-
 
 double euclidian_dist(double point_x, double point_y, double centr_x, double centr_y) {
     return sqrt(pow((point_x - centr_x),2) + pow((point_y - centr_y),2));
 }
 
-void assign_points(T_point * points, int points_len, T_cluster * clusters, int clusters_len) {
+void assign_point(double point[8][4], int point_id, double cluster[2][2], int cluster_id) {
     int itr_point = 0;
-    int itr_cluster = 0;
-    double old_ecl_dist = 0;
-    double new_ecl_dist = 0;
-
-    for(itr_cluster = 0; itr_cluster < clusters_len; itr_cluster++){
-        for(itr_point = 0; itr_point < points_len; itr_point++){
-            
-            old_ecl_dist = points[itr_point].ctr_dist;
-            
-            new_ecl_dist = euclidian_dist(points[itr_point].point_x, points[itr_point].point_y, 
-            clusters[itr_cluster].centr_x, clusters[itr_cluster].centr_x);
-
-            if(new_ecl_dist > old_ecl_dist){
-                points[itr_point].ctr_dist = new_ecl_dist;
-                
-                if(points[itr_point].cluster_id != -1){
-                    printf("Removed from cluster %d\n", points[itr_point].cluster_id);
-                    points[itr_point].cluster_id = -1;
-                }
-                
-                points[itr_point].cluster_id = clusters[itr_cluster].cluster_id;
-                printf("Added in cluster %d\n", points[itr_point].cluster_id);
-            }
+    double old_ecl_dist = 0.0;
+    double new_ecl_dist = 0.0;
+    old_ecl_dist = point[point_id][2];
+    new_ecl_dist = euclidian_dist(point[point_id][0], point[point_id][1], cluster[cluster_id][0], cluster[cluster_id][1]);
+    
+    if(new_ecl_dist < old_ecl_dist){
+        point[point_id][2] = new_ecl_dist;
+        
+        if(point[point_id][3] != 99.0){
+            printf("Removed from cluster %f\n", (int)point[point_id][3]);
+            point[point_id][3] = 99.0;
         }
-        itr_point = 0;
+        
+        point[point_id][3] = (double)cluster_id;
+        printf("Added in cluster %d\n", cluster_id);
     }
 }
 
-void update_centroids(T_point * points, int points_len, T_cluster * clusters, int cltrs_len) {
-    int itr_points, itr_cluster = 0;
+/*
+void update_centroids(int offset, T_point * points, int points_len, double centr_x, double centr_y, int cluster_id) {
+    int itr_points = 0;
     double sum_x, sum_y = 0.0;
     double n_cluster_pts = 0.0;
     double new_ctr_x, new_ctr_y = 0.0;
 
-    for(itr_cluster = 0; itr_cluster < cltrs_len; itr_cluster++){
-        sum_x = 0.0;
-        sum_y = 0.0;
-        new_ctr_x = 0.0;
-        new_ctr_y = 0.0;
-        n_cluster_pts = 0.0;
+    sum_x = 0.0;
+    sum_y = 0.0;
+    new_ctr_x = 0.0;
+    new_ctr_y = 0.0;
+    n_cluster_pts = 0.0;
+    
+    for(itr_points = offset; itr_points < points_len; itr_points++){
         
-        for(itr_points = 0; itr_points < points_len; itr_points++){
-            
-            if(points[itr_points].cluster_id == clusters[itr_cluster].cluster_id){
-                n_cluster_pts++;
-                sum_x += points[itr_points].point_x;
-                sum_y += points[itr_points].point_y;
-            }
+        if(points[itr_points].cluster_id == cluster_id){
+            n_cluster_pts++;
+            sum_x += points[itr_points].point_x;
+            sum_y += points[itr_points].point_y;
         }
-        new_ctr_x = sum_x / n_cluster_pts;
-        new_ctr_y = sum_y / n_cluster_pts;
+    }
+    new_ctr_x = sum_x / n_cluster_pts;
+    new_ctr_y = sum_y / n_cluster_pts;
 
-        if(new_ctr_x != clusters[itr_cluster].centr_x && new_ctr_y != clusters[itr_cluster].centr_y){
-            clusters[itr_cluster].centr_x = new_ctr_x;
-            clusters[itr_cluster].centr_y = new_ctr_y;
-            printf("Centroid from cluster %d changed!\n", itr_cluster);
-        }
+    if(new_ctr_x != centr_x && new_ctr_y != centr_y){
+        centr_x = new_ctr_x;
+        centr_y = new_ctr_y;
+        printf("Centroid from cluster %d changed!\n", cluster_id);
+    }
 
-        else {
-            printf("Centroid from cluster %d stayed the same...\n", itr_cluster);
-        }
-        
-        itr_points = 0;
+    else {
+        printf("Centroid from cluster %d stayed the same...\n", cluster_id);
     }
 }
+*/
 
-void print_clusters(T_point * points, int points_len, T_cluster * clusters, int clusters_len){
-    int itr_cluster = 0;
+void print_clusters(double point[8][4], double cluster[2][2]){
     int itr_points = 0;
-
-    for(itr_cluster = 0; itr_cluster < clusters_len; itr_cluster++){
-        printf("Elements from cluster %d:\n", itr_cluster);
-        for(itr_points = 0; itr_points < points_len; itr_points++){
-            
-            if(points[itr_points].cluster_id == clusters[itr_cluster].cluster_id){
-                printf("( %f, %f ) ", points[itr_points].point_x, points[itr_points].point_y);
+    int itr_clusters = 0;
+    
+    for (itr_clusters = 0; itr_clusters < 2; itr_clusters++){
+        printf("Elements from cluster %d:\n", itr_clusters);
+        for(itr_points = 0; itr_points < 8; itr_points++){
+            if(point[itr_points][3] == itr_clusters){
+                printf("( %f, %f ) ", point[itr_points][0], point[itr_points][1]);
             }
         }
+        itr_points = 0;
         printf("\n");
     }
 }
 
-int main () {
+int main (int argc, char *argv[]) {
+    int num_tasks, task_id, dest, source, offset, tag_1, tag_2, chunk_size;
+    MPI_Status status;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_tasks);
     
-    double points [7][2] = {{1.0, 1.0},
-                         {1.5, 2.0},
-                         {3.0, 4.0},
-                         {5.0, 7.0},
-                         {3.5, 5.0},
-                         {4.5, 5.0},
-                         {3.5, 4.5}};
-    T_point points_t [7];
-    
-    int itr = 0;
-    
-    for (itr = 0; itr < 7; itr++){
-        points_t[itr].point_x = points[itr][0];
-        points_t[itr].point_y = points[itr][1];
-        points_t[itr].ctr_dist = 0.0;
-        points_t[itr].cluster_id = -1;
-        points_t[itr].id = itr;
+    /*
+    check if array can be divided
+    by the number of tasks proposed
+    */
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &task_id);
+    //Message tags, tag 1 for offset and tag 2 for data_chunk
+    tag_1 = 1;
+    tag_2 = 2;
+
+    int cluster_t_size = 2;
+    int points_t_size = 8;
+
+    chunk_size = (points_t_size / num_tasks);
+ 
+     double points[8][4] = {{1.0, 1.0, 99.0, 99.0},
+                         {1.5, 2.0, 99.0, 99.0},
+                         {3.0, 4.0, 99.0, 99.0},
+                         {5.0, 7.0, 99.0, 99.0},
+                         {3.5, 5.0, 99.0, 99.0},
+                         {4.5, 5.0, 99.0, 99.0},
+                         {3.5, 4.5, 99.0, 99.0},
+                         {2.5, 1.5, 99.0, 99.0}};
+
+    double clusters_centroids[2][2] = {{0.0, 0.0},
+                                    {7.5, 7.5}};
+
+    // ============== MASTER =========================
+    if(task_id == 0){
+
+        offset = chunk_size;
+
+        //send each process its chunk
+        for (dest=1; dest < num_tasks; dest++){
+            MPI_Send(&offset, 1, MPI_INT, dest, tag_1, MPI_COMM_WORLD);
+            MPI_Send(&points[offset], chunk_size, MPI_DOUBLE, dest, tag_2, MPI_COMM_WORLD);
+            offset = offset + chunk_size;
+        }
+
+        offset = 0;
+
+        //printf("process id %d offset %d\n", task_id, offset);
+
+        int itr_p;
+        int itr_c;
+        for(itr_c = 0; itr_c < cluster_t_size; itr_c++) {
+            for (itr_p = offset; itr_p < chunk_size + offset; itr_p++){
+                assign_point(points, itr_p, clusters_centroids, itr_c);
+            }
+            itr_p = offset;
+        }
+        int tasks;
+        for(tasks = 1; tasks < num_tasks; tasks++){
+            MPI_Recv(&offset, 1, MPI_INT, tasks, tag_1, MPI_COMM_WORLD, &status);
+            MPI_Recv(&points[offset], chunk_size, MPI_DOUBLE, tasks, tag_2, MPI_COMM_WORLD, &status);
+        }
     }
-    
-    double clusters_centroids [2][2] = {{0.0,0.0},{7.5,7.5}};
 
-    T_cluster clusters_t [2];
-    
-    clusters_t[0].centr_x = clusters_centroids[0][0];
-    clusters_t[0].centr_y = clusters_centroids[0][1];
-    clusters_t[0].cluster_id = 0;
+    // ================== WORKER ======================
+    if(task_id > 0){
+        source = 0;
+        MPI_Recv(&offset, 1, MPI_INT, source, tag_1, MPI_COMM_WORLD, &status);
+        MPI_Recv(&points[offset], chunk_size, MPI_DOUBLE, source, tag_2, MPI_COMM_WORLD, &status);
 
-    clusters_t[1].centr_x = clusters_centroids[1][0];
-    clusters_t[1].centr_y = clusters_centroids[1][1];
-    clusters_t[1].cluster_id = 1;
+        //printf("process id %d offset %d\n", task_id, offset);
+        
+        int itr_p = 0;
+        int itr_c = 0;
+        for(itr_c = 0; itr_c < cluster_t_size; itr_c++) { 
+            for (itr_p = offset; itr_p < chunk_size + offset; itr_p++){
+                assign_point(points, itr_p, clusters_centroids, itr_c);
+            }
+            itr_p = offset;
+        }
+        dest = 0;
+        MPI_Send(&offset, 1, MPI_INT, dest, tag_1, MPI_COMM_WORLD);
+        MPI_Send(&points[offset], chunk_size, MPI_DOUBLE, dest, tag_2, MPI_COMM_WORLD);
 
-    int n_iter = 1;
-    
-    while(n_iter <= 5){
-        assign_points(points_t, 7, clusters_t, 2);
-        update_centroids(points_t, 7, clusters_t, 2);
-        n_iter++;
     }
 
-    print_clusters(points_t, 7, clusters_t, 2);
+    print_clusters(points, clusters_centroids);
 
+    MPI_Finalize();
     return 0;
 }
