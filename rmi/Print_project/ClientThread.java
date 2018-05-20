aebbce0bc8f02fceaa8fabab47c40e89ac9ec586
Eduardo Zimelewicz <eduardo.zimelewicz@gmail.com>
2018-05-19 21:26:25 -0300

    added threads

---
+++ rmi/Print_project/ClientThread.java
@@ -0,0 +1,29 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+public class ClientThread extends Thread {
+    private int id;
+
+    public ClientThread(int id){
+        this.id = id;
+    }
+
+    public void run(){
+        try {
+            if(System.getSecurityManager() == null){
+                System.setSecurityManager(new SecurityManager());
+            }
+    
+            String message = "Message from PrintClient " + id + "\n" ;
+            Print printer = (Print) Naming.lookup("//localhost/PrintServer");
+            System.out.println(printer.printMessage(message));
+        } 
+
+        catch (Exception e) {
+            System.out.println("Error looking up registry");
+            e.printStackTrace();
+        }
+    }
+}
--- rmi/Print_project/PrintClient.java
+++ rmi/Print_project/PrintClient.java
@@ -2,23 +2,27 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 public class PrintClient {
-    public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
+    public static ArrayList<Thread> clients = new ArrayList<Thread>();
+    public static ArrayList<Thread> buffer = new ArrayList<Thread>();
 
-        try {
-            if(System.getSecurityManager() == null){
-                System.setSecurityManager(new SecurityManager());
-            }
-    
-            String message = "Message from PrintClient\n";
-            Print printer = (Print) Naming.lookup("//localhost/PrintServer");
-            System.out.println(printer.printMessage(message));
-        } 
+    public static void main(String[] args) throws MalformedURLException, RemoteException, 
+        NotBoundException, InterruptedException {
+        
+        if(System.getSecurityManager() == null){
+            System.setSecurityManager(new SecurityManager());
+        }
 
-        catch (Exception e) {
-            System.out.println("Error looking up registry");
-            e.printStackTrace();
+        for (int itr = 0; itr < 5; itr++){
+            clients.add(new ClientThread(itr));
+        }
+
+        for (int itr = 0; itr < 5; itr++){
+            clients.get(itr).start();
+            clients.get(itr).sleep(1000);
+            System.out.println("Client Thread " + itr + " started");
         }
     }
 }
